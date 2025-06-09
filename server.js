c4175ec675d18013607eaccb4971d77909ac6f92
Angie C <a.cajusol98@gmail.com>
2025-06-10 00:36:10 +0200

    setup

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
 import pokemonRoutes from './routes/pokemonRoutes.js';
-import db from "./db.js";
 
 
 dotenv.config();
@@ -12,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/', (req, res) => {
+  res.send('Welcome to the Pokémon API!');
+}
+);
 app.use('/api/auth', authRoutes);
 app.use('/api/pokemon', pokemonRoutes);
 
